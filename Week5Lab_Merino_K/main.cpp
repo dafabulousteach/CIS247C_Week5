95fb9c004a1caddd3e162e22a65806f81ef9ee84
Kim Merino <dafabulousteach@gmail.com>
2015-02-03 14:51:32 -0800

    Initial Commit

---
+++ Week5Lab_Merino_K/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Week5Lab_Merino_K
+//
+//  Created by Kim Merino on 2/3/15.
+//  Copyright (c) 2015 Kim Merino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
